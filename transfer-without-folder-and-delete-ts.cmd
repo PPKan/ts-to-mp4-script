44d0298964e0cdbea3922a8b924c3c9b85061abb
Peter Work Laptop <petervsjim@gmail.com>
2023-04-27 08:50:00 +0800

    double quote argument of del

--- transfer-without-folder-and-delete-ts.cmd
+++ transfer-without-folder-and-delete-ts.cmd
@@ -4,7 +4,7 @@
 
 for %%f in (".\*.TS") do (
     :: 將 .TS 檔案進行轉換，並將結果儲存為具有相同檔名的 .mp4 檔案，並刪除in資料夾內的檔案
-    ffmpeg -i "%%f" -c copy -map 0:v -map 0:a -bsf:a aac_adtstoasc ".\%%~nf.mp4" & del %%f
+    ffmpeg -i "%%f" -c copy -map 0:v -map 0:a -bsf:a aac_adtstoasc ".\%%~nf.mp4" & del "%%f"
 )
 
 :: 完成轉換並退出
